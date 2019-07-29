3b649238bc5bd63f6cb70a14071ca15cc1a08233
Monali Chandurkar <chandurkar.monali123@gmail.com>
2019-07-29 00:41:34 -0700

    code works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,5 @@
+var correct=0;
+var incorrect=0;
 var questions = [
     {
 
@@ -52,6 +54,46 @@ var questions = [
        ans:144
 
 
+    },
+    {
+
+        qusetion:"Whats is 90*9?",
+       ans1:3,
+       ans2:144,
+       ans3:810,
+       ans:810
+
+
+    },
+    {
+
+        qusetion:"Whats is 1212/12?",
+       ans1:11,
+       ans2:144,
+       ans3:810,
+       ans:11
+
+
+    },
+    {
+
+        qusetion:"Whats is 88*88?",
+       ans1:3892,
+       ans2:7744,
+       ans3:8984,
+       ans:7744
+
+
+    },
+    {
+
+        qusetion:"Whats is 31*67?",
+       ans1:2077,
+       ans2:7744,
+       ans3:8984,
+       ans:2077
+
+
     }
    
      
@@ -64,21 +106,24 @@ var questions = [
 $(document).ready(function(){
 
 
+    $("#res").hide();
 
-
-    $("#questions_sec").css({"background-color":"yellow","height":"90vh","width":"50%","margin":"auto"});
-
-
+   
     $("#start").click(function(){
 
         $("#start").hide();
+       
 
         ques();
 
-    })
+    });
 
 
     var ques = function(){
+
+        $("#questions_sec").css({"background-color":"yellow","height":"100vh","width":"50%","margin":"auto"});
+
+
 
              var titile = $("<h1>");
     
@@ -91,86 +136,69 @@ $(document).ready(function(){
             timeRemain.text("Time Remaining");
 
             timeRemain.add("<span>").attr("id","timeSecon").appendTo(timeRemain);
+            
 
             $("#questions_sec").add(timeRemain).appendTo("#questions_sec");
 
             for(var i=0;i<questions.length;i++){
-                //console.log(questions[i].qusetion);
-                //console.log(questions[i].ans1);
+               
 
             var newQuesDiv= $("<div>");
 
             var que = $("<p>");
      
             que.text(questions[i].qusetion);
+            que.attr("id",i.toString());
             
      
             var ansDiv = $("<div>");
-     
+            console.log(questions[i].ans1);
+
+            var btn1 = $('<input>');
+            btn1.attr("type","radio");
+            btn1.attr("value",questions[i].ans1);
+            btn1.attr("name","que"+i);
+            var p1 = $('<span>');
+            p1.text(questions[i].ans1);
+            p1.append(btn1);
+            ansDiv.append(p1);
+
+            var btn2 = $('<input>');
+            btn2.attr("type","radio");
+            btn2.attr("name","que"+i);
+            btn2.attr("value",questions[i].ans2);
+            var p2 = $('<span>');
+            p2.text(questions[i].ans2);
+            p2.append(btn2);
+            ansDiv.append(p2);
+
+            var btn3 = $('<input>');
+            btn3.attr("type","radio");
+            btn3.attr("name","que"+i);
+            btn3.attr("value",questions[i].ans3);
+            var p3 = $('<span>');
+            p3.text(questions[i].ans3);
+            p3.append(btn3);
+            ansDiv.append(p3);
             
-             var radioBtn1 = $('<input type="radio" name="rbtnCount" value=""/>');
-             radioBtn1.text(questions[i].ans1);
-             radioBtn1.attr("index",i);
-             radioBtn1.append("<h5>"+questions[i].ans1+"</h5>");
-             ansDiv.append(radioBtn1);
-             
-
-             var radioBtn2 = $('<input type="radio" name="rbtnCount" />');
-             radioBtn2.text(questions[i].ans2);
-             ansDiv.append(radioBtn2);
-             radioBtn2.attr("index",i);
-
-             var radioBtn3 = $('<input type="radio" name="rbtnCount" />');
-             radioBtn3.text(questions[i].ans3);
-             ansDiv.append(radioBtn3);
-             radioBtn3.attr("index",i);
+           
      
             newQuesDiv.append(que);
             que.append(ansDiv);
      
             $("#questions_sec").append(newQuesDiv);
 
-            
-            
             }
-
-
-            
-
-               /* alert("clicked");
-
-                var ansVal = $("input[type='radio']:checked").text();
-
-                //console.log($("input[type='radio']:checked"));
-
-                console.log(ansVal);
+        }
 
         
-                console.log("in change");
+            
         
-            })*/
-
-            var checkRes = function(){
-                var index = $(this).attr("index");
-                console.log(questions[index].qusetion);
-                var ansVal = $("input[type='radio']:checked").text();
-                console.log(ansVal);
-                if(ansVal==questions[index].ans){
-                    console.log("true");
-                }
-
-               
-            }
-
-            $("input[type='radio']").click(".index", checkRes);
-
-            
-
-
-            
+        
 
            
-            var number = 3;
+            var number = 10;
+            
 
             var intervalID;
 
@@ -187,17 +215,32 @@ $(document).ready(function(){
                 
                 if (number === 0) {
                   //  Alert the user that time is up.
+                  $("#res").show();
 
                   $("#questions_sec").hide();
 
                   $("#res").css({"background-color":"yellow","height":"30vh","width":"20%","margin-top":"10%","margin-left":"40%"});
-
+                  $('#correctP').css({"margin-top":"20%","margin-left":"30%","padding-top":"20%"});
+                  $('#incorrectP').css({"margin-left":"30%"});
+                  for(var i=0;i<questions.length;i++){
+                    var selValue = $('input[name=que'+i+']:checked').val(); 
+                    if(questions[i].ans==selValue){
+                        correct++;
+                    }
+                    else{
+                        incorrect++;
+                    }
+                  }
+                  $("#correct").text(correct);
+                  $('#incorrect').text(incorrect);
+                  console.log("Correct "+correct);
+                  console.log("Incorrect "+incorrect);
                   
                 }
               }
           
        
-    }
+    
 
     
 
